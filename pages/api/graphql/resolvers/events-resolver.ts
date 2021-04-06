47c4369e2ea88e86b21390c975c7870dd290edcc
Muhammad Omran <omran@radivision.com>
2021-04-06 18:57:52 +0200

    finishing a basic working boilerplate project using next, typescript, graphql, mongodb with posts and users models

--- pages/api/graphql/resolvers/events-resolver.ts
+++ pages/api/graphql/resolvers/events-resolver.ts
@@ -6,16 +6,19 @@ export const eventsResolver = {
     getEvents: async (): Promise<EventResponse[]> => {
       try {
         const eventsCursors = await EventModel.find();
-        return eventsCursors.map((e: EventCursor): EventResponse => {return {
-          id: e._id,
-          title: e.title,
-          description: e.description,
-          user: e.user,
-          createdAt: e.createdAt
-        }});
-      }
-      catch (err) {
-        throw new Error (err);
+        return eventsCursors.map(
+          (e: EventCursor): EventResponse => {
+            return {
+              id: e._id,
+              title: e.title,
+              description: e.description,
+              user: e.user,
+              createdAt: e.createdAt,
+            };
+          }
+        );
+      } catch (err) {
+        throw new Error(err);
       }
     },
   },
