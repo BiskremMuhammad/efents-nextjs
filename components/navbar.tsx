5eaecd7768816c752d417c2820a80b03a8881116
Muhammad Omran <use.yurMind@gmail.com>
2021-04-20 15:30:06 +0200

    update navbar styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,20 +4,22 @@ import { EfentsLogo } from "./efents-logo";
 export const Navbar = () => {
   return (
     <nav>
-      <EfentsLogo />
-      <ul>
-        <li>
-          <a>Events</a>
-        </li>
-        <li>
-          <a>Calendar</a>
-        </li>
-        <li>
-          <a>Create Event</a>
-        </li>
-      </ul>
-      <div className="user-area">
-        <span>Login</span>
+      <div className="navbar-container">
+        <EfentsLogo />
+        <ul>
+          <li>
+            <a>Events</a>
+          </li>
+          <li>
+            <a>Calendar</a>
+          </li>
+          <li>
+            <a>Create Event</a>
+          </li>
+        </ul>
+        <div className="user-area">
+          <span>Login</span>
+        </div>
       </div>
     </nav>
   );
