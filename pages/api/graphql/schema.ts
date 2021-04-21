e828dcba4b4d6ce07f436876cd9d418b34554018
Muhammad Omran <use.yurMind@gmail.com>
2021-04-21 15:55:39 +0200

    update user schema

--- pages/api/graphql/schema.ts
+++ pages/api/graphql/schema.ts
@@ -1,17 +1,45 @@
 import { gql } from "apollo-server-micro";
 
 export const schema = gql`
+  enum Gender {
+    MALE
+    FEMALE
+  }
+  enum Language {
+    ARABIC
+    ENGLISH
+  }
+  type Privacy {
+    enableFollow: Boolean
+    enableSchedule: Boolean
+  }
+  type SocialLinks {
+    facebook: String
+    twitter: String
+    website: String
+  }
   type User {
     id: String!
     username: String!
     email: String!
-    firstName: String
+    firstName: String!
     lastName: String
-  },
+    displayName: String!
+    gender: Gender!
+    avatar: String
+    social: SocialLinks
+    followers: [String]!
+    followings: [String]!
+    events: [String]!
+    schedule: [String]!
+    language: Language!
+    privacy: Privacy
+  }
   type Event {
     id: String!
     title: String!
     description: String
+    hasTranslation: String
     createdAt: String!
     user: String!
   }
