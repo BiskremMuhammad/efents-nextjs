48f7045d43925dc43e6a36dbf3196062771398a0
Muhammad Omran <use.yurMind@gmail.com>
2021-04-25 14:13:50 +0200

    define type definitions for db relations

--- pages/api/models/relations.ts
+++ pages/api/models/relations.ts
@@ -1,3 +1,8 @@
+/**
+ * @author Muhammad Omran
+ * @date 25-04-2021
+ * @description implments database relations models
+ */
 import { Schema, model, models, Document } from "mongoose";
 
 /**
@@ -25,6 +30,29 @@ export const followTable =
   );
 
 /**
+ * interface that defines the Follow Cursor
+ *
+ * @interface
+ * @exports
+ * @extends Document
+ */
+export interface FollowCursor extends Document {
+  /**
+   * the id of the user who is being followed
+   *
+   * @type {string}
+   */
+  follow: string;
+
+  /**
+   * the id of the user who made the follow action
+   *
+   * @type {string}
+   */
+  userId: string;
+}
+
+/**
  * defienes the goings to event which will have key value pair
  * which userId: the user is the subject >> who made the action
  * and event: is the Object >> which users are going to
@@ -47,3 +75,26 @@ export const goingTable =
       { timestamps: true }
     )
   );
+
+/**
+ * interface that defines the going to event Cursor
+ *
+ * @interface
+ * @exports
+ * @extends Document
+ */
+export interface EventGoingCursor extends Document {
+  /**
+   * the id of the event
+   *
+   * @type {string}
+   */
+  event: string;
+
+  /**
+   * the id of the user who is going to the event
+   *
+   * @type {string}
+   */
+  userId: string;
+}
